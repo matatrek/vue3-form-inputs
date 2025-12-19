74f783f21a1c1cd21fa535544c3a3b9dba6bf1ec
EdwinJMataH <ejmh.developer@gmail.com>
2025-12-19 16:10:36 -0600

    chore: add path aliases for headless architecture

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@src': path.resolve(__dirname, './src')
+      '@src': path.resolve(__dirname, './src'),
+      '@headless': path.resolve(__dirname, './src/headless'),
+      '@composables': path.resolve(__dirname, './src/composables'),
+      '@plugins': path.resolve(__dirname, './src/plugins'),
+      '@theme': path.resolve(__dirname, './src/theme'),
     }
   },
   root: './playground',
