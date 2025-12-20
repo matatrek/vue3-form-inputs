a44f006d6ac4646943641faf4b7b9779ca4c8724
EdwinJMataH <ejmh.developer@gmail.com>
2025-12-19 22:14:37 -0600

    feat: add FormContainer to provide theme context

--- src/components/FormContainer.vue
+++ src/components/FormContainer.vue
@@ -1,60 +1,58 @@
 <script setup lang="ts">
-import { useVuelidate } from '@vuelidate/core';
-import { ref, defineExpose, defineProps, defineEmits, provide, withDefaults, computed } from 'vue';
-import { useTranslation } from '@src/utils/useTranslation';
-import { useTheme } from '@src/theme/index'
+import { useVuelidate } from "@vuelidate/core";
+import {
+  ref,
+  defineExpose,
+  defineProps,
+  defineEmits,
+  provide,
+  withDefaults,
+  computed,
+} from "vue";
+import { useTheme, cx } from "@composables/useTheme.ts";
 
 interface Props {
   form: Record<string, any>;
   rules: Record<string, any>;
   wrapperForm?: string;
-  titleButton?: string;
-  iconButton?: any;
+  themeOverride?: Record<string, string>;
 }
-const props = withDefaults(defineProps<Props>(), {
-  titleButton: 'send'
-});
+const props = defineProps<Props>();
 
 const emit = defineEmits<{
-  (event: 'submit', isValid: boolean): void;
+  (event: "submit", isValid: boolean): void;
 }>();
 
-const rulesVuelidate  = ref(props.rules);
-const $v              = useVuelidate(props.rules, props.form);
-const { t }           = useTranslation();
-const theme           = useTheme()
+const rulesVuelidate = ref(props.rules);
+const $v = useVuelidate(props.rules, props.form);
+const theme = useTheme("form", props.themeOverride);
 
-provide('rulesVuelidate', rulesVuelidate);
-provide('errorsVuelidate', computed(() => $v.value));
+provide("rulesVuelidate", rulesVuelidate);
+provide(
+  "errorsVuelidate",
+  computed(() => $v.value)
+);
 
 const reset = () => {
   $v.value.$reset();
-}
+};
 
 const validate = async () => {
   const isValid = await $v.value.$validate();
-  emit('submit', isValid);
+  emit("submit", isValid);
   return isValid;
-  // console.log("errors", $v.value);
-  // $v.value.$touch();
-  // emit('submit', !$v.value.$error)
-  // return !$v.value.$error;
 };
 
 // Exponer métodos y datos al padre
 defineExpose({ validate, reset });
-
 </script>
 
 <template>
-  <form @submit.prevent="validate" :class="['mtk-form', theme.form]">
-    <slot/>
+  <form @submit.prevent="validate" :class="cx(theme.wrapper)">
+    <slot />
 
-    <slot name="options">
-      <button type="submit" :class="['mtk-form-button', theme.formButton]">
-        <component v-if="iconButton" :is="iconButton" :class="['mtk-form-button-icon', theme.formButtonIcon]"/>
-        {{ t(titleButton) }}
-      </button>
-    </slot>
+    <div v-if="$slots.actions" :class="cx(theme.actions)">
+      <slot name="actions" />
+    </div>
   </form>
 </template>
