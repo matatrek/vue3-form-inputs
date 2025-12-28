2fb22b28f73bd6290dcd7bd8753d593b7a2a0f55
EdwinJMataH <ejmh.developer@gmail.com>
2025-12-27 18:25:50 -0600

    refactor!: compose UX, validation and form state via composables

--- src/headless/BaseInput.vue
+++ src/headless/BaseInput.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { defineProps, defineEmits, withDefaults, defineSlots } from "vue";
 import type { BaseInputSlotProps, Props } from "@src/types/base-input.ts";
-import { useBaseInput } from "@composables/useBaseInput.ts";
+import { useBaseInput } from "@composables/useBaseInput";
+import { useInputValidation } from "@composables/useInputValidation";
+import { useFormState } from "@composables/useFormState";
+
 
 const props = withDefaults(defineProps<Props>(), {
   type: "text",
@@ -12,20 +15,37 @@ const props = withDefaults(defineProps<Props>(), {
 
 const emit = defineEmits<{
   (event: "update:modelValue", value: string): void;
+  (event: "input", e: Event): void;
 }>();
 
 defineSlots<{
   default?: { input: BaseInputSlotProps };
 }>();
+const { isFormSubmitted } = useFormState();
 
-const { isRequired, hasError, message, onUpdate } = useBaseInput(props, emit);
+const {
+  isFocused,
+  isTouched,
+  isDirty,
+  onUpdate,
+  onFocus,
+  onBlur,
+  onInput,
+} = useBaseInput(emit);
+
+const {
+  isRequired,
+  hasError,
+  message,
+} = useInputValidation(props.validation);
 </script>
 
 <template>
   <slot
     :input="{
-      id: id,
-      name: name,
+      id,
+      name,
+      length,
       value: modelValue,
       type: type,
       placeholder: placeholder,
@@ -34,7 +54,14 @@ const { isRequired, hasError, message, onUpdate } = useBaseInput(props, emit);
       isRequired,
       hasError,
       message,
+      isFocused,
+      isTouched,
+      isDirty,
       onUpdate,
+      onFocus,
+      onBlur,
+      onInput,
+      isFormSubmitted
     }"
   />
 </template>
